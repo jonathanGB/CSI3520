7c31ae156a5be7b0a97f931cdf36ddb12ea8a54a
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2016-09-21 19:28:29 -0400

    heapsort function looks alright, need to implement sub-algorithms

--- heapsort.scm
+++ heapsort.scm
@@ -4,12 +4,21 @@
 ; Courriel d’étudiant: jguil098@uottawa.ca
 
 (define (heapsort l)
-  (let ((count (length l)))
-    (begin
-      (heapify l count)
+  (let* ((count (length l)) (endIndex (- count 1)) (heap (heapify l count)))
+    (letrec ((whilePositive (lambda (i arr)
+      (if (> i 0)
+        (whilePositive (- i 1) (swap arr i))
+        arr
+      ))))
       ; recursive call to swap and siftDown
+      (whilePositive endIndex heap)
     )
   )
+)
+
+
+(define (swap arr i)
+  (begin (write i) arr)
 )
 
 
