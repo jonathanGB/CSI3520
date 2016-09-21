b538131ac74aded9351292ea1415c20c71176ad0
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2016-09-21 18:45:56 -0400

    started heapsort function

---
+++ heapsort.scm
@@ -0,0 +1,23 @@
+; CSI 3520 Devoir 1 Question 1
+; Nom de l'élève : Jonathan Guillotte-Blouin
+; Nombre d'étudiants : 7900293
+; Courriel d’étudiant: jguil098@uottawa.ca
+
+(define (heapsort l)
+  (let ((count (length l)))
+    (begin
+      (heapify l count)
+      ; recursive call to swap and siftDown
+    )
+  )
+)
+
+
+(define (heapify l count)
+  (display count)
+)
+
+
+(define (siftDown l startIndex endIndex)
+  (display startIndex)
+)
