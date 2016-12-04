f7ec0a82664b1b2da50e3e8da56e865f77f46395
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2016-12-04 03:34:06 -0500

    finished part 2

--- assignment4/7900293.r
+++ assignment4/7900293.r
@@ -33,3 +33,46 @@ generateNewYear = function() {
 
   sellsPerYear <<- unlist(list(sellsPerYear, list(sellsMatrix)), recursive=FALSE)
 }
+
+
+# Part 2
+printOrder = function(m) {
+  return(paste("->", m[1], "AC\n ->", m[2], "appliances\n ->", m[3], "toys"))
+}
+
+checkStocks = function(yearIndex) {
+  if (yearIndex <= 0 || yearIndex > length(sellsPerYear)) {
+    return(NULL)
+  }
+
+  sellsThisYear = sellsPerYear[[yearIndex]]
+  stocks = matrix(
+    c(10, 10, 10, 10, 10, 30, 50, 50, 30, 10, 10, 10,
+      40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40,
+      40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 400, 400
+    ),
+    nrow  = 3,
+    ncol  = 12,
+    byrow = TRUE
+  )
+  monthBucket = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+  # write.matrix(stocks)
+  # cat("\n")
+  # write.matrix(sellsThisYear)
+  # cat("\n\n\n")
+
+  for (i in 1:12) {
+    monthStocks     <- stocks[, i]
+    nextMonthStocks <- stocks[, (i %% 12) + 1]
+    monthSells      <- sellsThisYear[, i]
+    monthDiff       <- ifelse(monthStocks - monthSells < 0, 0, monthStocks - monthSells) # doesn't allow more sells than current stock
+    nextOrder       <- ifelse(nextMonthStocks - monthDiff < 0, 0, nextMonthStocks - monthDiff) # don't refill stocks if current stock is bigger than next month
+
+    stocks[, (i %% 12) + 1] <- ifelse(nextOrder == 0, monthDiff, nextMonthStocks) # if current stock bigger then next month, update next month stock
+
+    cat(monthBucket[i], "\n---------------\n", printOrder(nextOrder), "\n\n") # print order
+  }
+
+  # cat("\n\n")
+  # write.matrix(stocks)
+}
